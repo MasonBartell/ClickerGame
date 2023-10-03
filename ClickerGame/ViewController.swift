e627bd94e2a24247d3af04a7955efc008a318022
Mason Bartell <masonjbartell@gmail.com>
2023-10-03 09:09:26 -0500

    Initial Commit

---
+++ ClickerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerGame
+//
+//  Created by MASON BARTELL on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
